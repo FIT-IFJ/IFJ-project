1591877531f69dbc46e56a786d7f542f1437b819
Natalia Bubakova <nbubakov@redhat.com>
2021-11-25 21:04:26 +0100

    DLL struktura

--- structures.c
+++ structures.c
@@ -67,7 +67,6 @@ void DLL_InsertLast( DLList *list, token_t *token )
     }
     new_element->token = (token_t *) malloc(sizeof(token_t));
     * new_element->token = *token;
-    printf("**%s**\n", new_element->token->attribute);
     new_element->next = NULL;
     new_element->prev = list->last;
     if(list->last != NULL)
@@ -119,13 +118,16 @@ void DLL_DeleteLast( DLList *list )
 
 void DLL_Dispose( DLList *list )
 {
-    list->active = list->first;
-    while (list->active != NULL) 
+    DLLElement *element = list->first;
+    DLLElement *del_element;
+    while (element != NULL) 
     {
-        free(list->active->token);
-        free(list->active);
-        list->active = list->active->next;
+        del_element = element;
+        element = element->next;
+        free(del_element->token);
+        free(del_element);
     }
+
     list->active = NULL;
     list->first = NULL;
     list->last = NULL;
@@ -336,6 +338,46 @@ int DLL_IsActive( DLList *list )
 // ***************************************************************************
 
 
+void parse_expression(DLList *pp_list)
+{
+    DLLElement *element = pp_list->first;
+    printf("\n******************\n\n");
+    while(element != NULL)
+    {
+        printf("processed[%s]\n", element->token->attribute);
+        element = element->next;
+
+    }
+}
+
+void DLL_fill(DLList *list, token_t *token, dynamic_string *string) // funkcia je zavolana po tokene "if", "while", "return" a "="
+{
+    DLL_Dispose(list);
+    while (token->type != TYPE_EOF)
+    {
+        get_token(token,string);
+        DLL_InsertLast(list, token);
+        printf("inserted[%s]\n", token->attribute);
+
+
+
+        if (token->type == TYPE_KEYWORD)        //      zakomentuj pre naplnenie DLL az do konca (nerobi to potom tolko chyb XD)
+            return;                             //
+        
+    }
+}
+
+
+
+
+// ***********************************************************************************************************************************************************************
+
+//                                                       HERE IT BEGINS
+
+// ***********************************************************************************************************************************************************************
+
+
+
 
 
 
@@ -348,33 +390,45 @@ void DLL_to_DLL(DLList *pp_list, DLList *list, token_t* token)
 }
 
 
-/*
-// vopred initializuj list
-//DLList *list = (DLList *)malloc (sizeof(DLList));
-//DLL_Init(list);
-void DLL_parse(DLList *list, token_t *token, dynamic_string *string) // funkcia je zavolana po tokene "if", "while", "return" a "="
+
+
+/**
+ * Funkcia je zavolana z parseru po tokene "if", "while", "return" a "=".
+ * Naplni pp_list a posunie ho do PSA parseru funkciou parse_expression(pp_list)
+ * Ak narazi na klucove slovo alebo situaciu vylucujucu vyskyt vyrazu, tokeny nepatriace
+ * do pp_listu uberie a prida do listu, z ktoreho si to vyberie hlavny parser.
+ * 
+ * @param list Ukazatel na inicializovanou DLL strukturu, ktora sa plni pre parser
+ * @param token Ukazatel na inicializovanu strukturu tokenu pre funkciu get_token(..)
+ * @param string --||--
+ */
+void DLL_parse(DLList *list, token_t *token, dynamic_string *string) 
 {
-    DLL_Dispose(list);
-    DLList *pp_list = (DLList *) malloc (sizeof(DLList));
+    DLL_Dispose(list);      // nwm ci tu musi byt, ale presitotu premazat pri znovu-uzivani
+    DLList *pp_list = (DLList *) malloc (sizeof(DLList));   //pp_list pre parsovanie vyrazu
     DLL_Init(pp_list);
 
-
-    while(token != NULL)
+    while(token->type != TYPE_EOF)
     {
         get_token(token, string);
+
         DLL_InsertLast(pp_list, token);
-        if (token->type == TYPE_KEYWORD || token->type == TYPE_DATATYPE)
+
+         //ak nasleduje keyword, tak vymaze posledny token a vlozi ho do listu pre hlavny parser
+        if (token->type == TYPE_KEYWORD || token->type == TYPE_DATATYPE)     
         {
             DLL_to_DLL(pp_list, list, token);
             break;
         }
         else if( list->last->prev != NULL )
         {
+             //ak nasleduje dalsi identifier bez znamienka medzi nimi, tak vymaze posledny token a vlozi ho do listu pre hlavny parser
             if (token->type == TYPE_IDENTIFIER && list->last->prev->token->type == TYPE_IDENTIFIER)
             {
                 DLL_to_DLL(pp_list, list, token);
                 break;
             }
+             //ak nasleduje funkcia, tak vymaze posledne dva tokeny a vlozi ho do listu pre hlavny parser
             else if (token->spec == SPEC_OPEN && list->last->prev->token->type == TYPE_IDENTIFIER)
             {
                 DLL_to_DLL(pp_list, list, token);
@@ -382,23 +436,18 @@ void DLL_parse(DLList *list, token_t *token, dynamic_string *string) // funkcia 
                 break;
             }
         }
-    
     }
-    if (pp_list->last != NULL)
-        parse_expression(pp_list);
-    DLL_Dispose(pp_list);
 
-
-
+    if (list->last != NULL)
+        parse_expression(pp_list);      //docasny vypis pre znazornenie prace PSA parseru
+    DLL_Dispose(pp_list);               // vymazanie pp_listu po tom ako su vyrazy spracovane
+    free(pp_list);
 }
-*/
+
+// ***************************************************************************
 
 
-
-
-
-
-int main()
+int main()  // testovaci main
 {
     DLList *list = (DLList *)malloc (sizeof(DLList));
     token_t* token = malloc(sizeof(token_t));
@@ -406,28 +455,76 @@ int main()
         return -1;
 
     dynamic_string *string = string_init();
-    printf("here\n");
+    
     DLL_Init(list);
-    printf("and here\n");
-    for (int i = 0; i < 10; i++)
+    
+    do
     {
         get_token(token,string);
-        DLL_InsertLast(list, token);
-        printf("[%s]\n", token->attribute);
-    }
-    
 
-    DLLElement *element = list->first;
-    printf(">>>%s<<<\n", element->token->attribute);
-    while(element != NULL)
-    {
-        printf("%s\n", element->token->attribute);
-        element = element->next;
+        printf("\n---> %s,%d,%d\n",token->attribute,token->type,token->spec);
 
-    }
+        if(token->spec == SPEC_RETURN || token->spec == SPEC_IF || token->spec == SPEC_WHILE || token->type == TYPE_ASSIGNMENT)
+        {
+            //DLL_fill(list, token, string);      // testovacie naplnenie DLL
+            //parse_expression(list);             // testovaci vypis DLL
+            DLL_parse(list, token, string);
+
+        }
+    }while (token->type != TYPE_EOF);
+
+
+    printf("\n\nKONIEC\n");
     DLL_Dispose(list);
     free (list);
     free (token);
 
     return 0;
 }
+
+
+
+
+
+
+
+/*
+int main()  // tento main uz znazornuje pracu parseru a mal by robit (ale nerobi) to co chcem 
+{
+    DLList *list = (DLList *)malloc (sizeof(DLList));
+    token_t* token = malloc(sizeof(token_t));
+    if (list == NULL || token == NULL)
+        return -1;
+
+    dynamic_string *string = string_init();
+    DLLElement *element;
+    DLL_Init(list);
+
+    do
+    {
+        get_token(token,string);
+        printf("\n---> %s,%d,%d\n",token->attribute,token->type,token->spec);   // cinnost parseru
+
+        if (token->spec == SPEC_RETURN || token->spec == SPEC_IF || token->spec == SPEC_WHILE || token->type == TYPE_ASSIGNMENT)
+        {
+            DLL_parse(list,token,string);   // volanie pre parsovanie pripadneho vyrazu
+            if (list->last != NULL)
+            {
+                element = list->first;
+                while(element != NULL)      // v listu sa nachadzaju omylom ukradnute tokeny pre parser
+                {
+                    printf("\n---> %s,%d,%d\n",element->token->attribute,element->token->type,element->token->spec);    // cinnost parseru
+                    element = element->next;
+                }
+            }
+        }
+    }while (token->type != TYPE_EOF);
+
+    
+    DLL_Dispose(list);
+    free (list);
+    free (token);
+
+    return 0;
+}
+*/
