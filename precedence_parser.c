c34b41d92e145ac5b5baaf86ff3e227f350ef636
Natalia Bubakova <nbubakov@redhat.com>
2021-12-03 16:52:30 +0100

    Syntax analyzer within precedence parser is done and ready to use (still with debug prints so you can see how it works)

--- precedence_parser.c
+++ precedence_parser.c
@@ -563,10 +563,10 @@ int parser()  // tento main uz znazornuje pracu parseru a mal by robit (ale nero
 
 
 //  ODKOMENTUJ PRE TESTOVANIE
-///*
+/*
 int main()
 {
     return parser();
 }
-//*/
+*/
 
