7bed01857bea2ab809009976b488c0705024c1f0
Tademait <tademait@github.com>
2021-12-08 22:30:07 +0100

    added an updated parser.h library

--- parser.h
+++ parser.h
@@ -17,37 +17,38 @@
 
 
 
-int program(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* AST);
-int prologue(token_t* token, token_t* token_lookahead, dynamic_string* string);
-int program_body(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int func_decl(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int func_def(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int func_call(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int type_list(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int types(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int type(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int param_list(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int params(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int constant_list(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int constants(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int func_body(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int decl_element(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int if_element(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int while_element(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int return_element(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int func_element(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int item_list(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int items(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int item(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int arg_list(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int args(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int arg(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int decl_assign(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int L_assignment(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int R_assignment(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int assignment(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int ids(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
-int return_list(token_t* token, token_t* token_lookahead, dynamic_string* string, ast_node_t* parent_node);
+int program(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* AST);
+int prologue(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string);
+int program_body(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int func_decl(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int func_def(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int func_call(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int type_list(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int types(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int type(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node, char* temp_var_name);
+int param_list(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int params(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int constant_list(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int constants(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int func_body(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int decl_element(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int if_element(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int while_element(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int return_element(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int func_element(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int item_list(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int items(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int item(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int arg_list(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int args(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int arg(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int decl_assign(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int L_assignment(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int R_assignment(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int assignment(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int ids(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
+int return_list(token_t* token, token_t* token_lookahead, dynamic_string*  dyn_string, ast_node_t* parent_node);
 
+void import_builtins();
 
 #endif
